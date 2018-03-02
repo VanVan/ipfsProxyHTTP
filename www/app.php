c0a03c38eecaec6d058772dee567f7884070b2a8
Wandrille RONCE <w@ndrille.fr>
2018-03-02 17:00:00 +0100

    Update License

--- www/app.php
+++ www/app.php
@@ -4,14 +4,14 @@
  *
  * NOTICE OF LICENSE
  *
- * This source file is subject to the Open Software License (OSL 3.0)
+ * This source file is subject to the MIT License
  * that is bundled with this package in the file LICENSE.txt.
  * It is also available through the world-wide-web at this URL:
- * https://opensource.org/licenses/OSL-3.0
+ * http://www.opensource.org/licenses/mit-license.html
  *
  * @author    Wandrille RONCE <contact@ipns.co>
  * @copyright 2017-2018 ipns.co
- * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
+ * @license http://www.opensource.org/licenses/mit-license.html  MIT License
  */
 require(dirname(__FILE__) . '/src/config.inc.php');
 
